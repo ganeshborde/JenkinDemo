89fd72ff866ae74e924f071ef4ee6b32abaf6531
ganeshborde <52167545+ganeshborde@users.noreply.github.com>
2026-03-12 10:58:02 +0530

    Modify welcome message and fix typos
    Updated welcome message and corrected typos.

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,6 @@
  public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Welcome+ Jay Shree Ram");
+        System.out.println("Welcome");
 		        System.out.println("I ruunned the program on jenkig new build  aded git desktop change");
 
     }
