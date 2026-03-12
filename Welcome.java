41b5a622182afb5fdfb48af55dc81854f98367d1
ganeshborde <52167545+ganeshborde@users.noreply.github.com>
2026-03-12 11:08:09 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,6 @@
  public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        System.out.println("Welcome check one minute built");
 		        System.out.println("I ruunned the program on jenkig new build  aded git desktop change");
 
     }
